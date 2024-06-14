a699b773149ad3bf9a5ba3319909be2c758c8092
Vincent Franstyo <sorakiriko28814@gmail.com>
2024-06-14 12:48:27 +0700

    feat(logic): added statistic basic logic

--- src/services/Homepage/statistics.services.ts
+++ src/services/Homepage/statistics.services.ts
@@ -1,20 +1,12 @@
 import {AuthenticatedRequest} from "../../utils/interface";
 import dotenv from 'dotenv';
-import {Request, Response} from 'express';
+import {Response} from 'express';
 import UserModel from "../../models/user.model";
+import ConsumptionModel from "../../models/consumption.model";
 
 dotenv.config()
 
 export const showUserStatistics = async (req: AuthenticatedRequest, res: Response) => {
-    // {
-    //     "consumed_count": 10,
-    //     "in_stock_count": 10,
-    //     "expired_count": 10,
-    //     "current_progress": 90,
-    //     "remaining_days": 2,
-    //     "history_progress": 80
-    // }
-
     try {
         if (!req.userId) {
             return res.status(400).json({error: 'User not found'});
@@ -25,6 +17,14 @@ export const showUserStatistics = async (req: AuthenticatedRequest, res: Respons
         if (!user) {
             return res.status(400).json({error: 'User not found'});
         }
+
+        const consumptions = await ConsumptionModel.getConsumptionsByUserId(req.userId);
+
+        let consumed_count = 0;
+        let in_stock_count = 0;
+        let expired_count = 0;
+        let current_progress = 0; // consumed / (consumed + in_stock + expired) * 100
+
 
         res.send({
             consumed_count: 10,
